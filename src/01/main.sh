b0965f0fd5fbda54709288fd69b1783015d4ac1c
MikhailPimenov <misha4git@yandex.ru>
2022-09-30 23:16:51 +0300

    develop/part1 - is ready, not tested with ubuntu in virtual box

---
+++ src/01/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input=$1
+regex_for_number='^[+-]?[0-9]+([.][0-9]+)?$'
+
+if [[ $input =~ $regex_for_number ]] ; then
+    bash output_error.sh
+else
+    bash output.sh $input
+fi
+
+
