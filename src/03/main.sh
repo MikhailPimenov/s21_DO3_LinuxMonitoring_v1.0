aa74068650faa2ced58a9dae341c0ec6affc9e25
MikhailPimenov <misha4git@yandex.ru>
2022-10-30 17:35:02 +0300

    develop/part3 - input check is added. Files for part5 are added

--- src/03/main.sh
+++ src/03/main.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+
+
+if [[ ($1 -ne 1) && ($1 -ne 2) && ($1 -ne 3) && ($1 -ne 4) && ($1 -ne 5) && ($1 -ne 6)]] ; then
+    echo 'Not correct input!'
+    echo 'Run this script again with exact four numbers 1,2,3,4,5 or 6!'
+    exit 1
+fi
+
+if [[ ($2 -ne 1) && ($2 -ne 2) && ($2 -ne 3) && ($2 -ne 4) && ($2 -ne 5) && ($2 -ne 6)]] ; then
+    echo 'Not correct input!'
+    echo 'Run this script again with exact four numbers 1,2,3,4,5 or 6!'
+    exit 1
+fi
+
+if [[ ($3 -ne 1) && ($3 -ne 2) && ($3 -ne 3) && ($3 -ne 4) && ($3 -ne 5) && ($3 -ne 6)]] ; then
+    echo 'Not correct input!'
+    echo 'Run this script again with exact four numbers 1,2,3,4,5 or 6!'
+    exit 1
+fi
+
+if [[ ($4 -ne 1) && ($4 -ne 2) && ($4 -ne 3) && ($4 -ne 4) && ($4 -ne 5) && ($4 -ne 6)]] ; then
+    echo 'Not correct input!'
+    echo 'Run this script again with exact four numbers 1,2,3,4,5 or 6!'
+    exit 1
+fi
+
+
+
 if [[ $1 -eq $2 ]] ; then
     echo 'Name background color and name font color are matching!'
     echo 'Run this script again with different colors!'
